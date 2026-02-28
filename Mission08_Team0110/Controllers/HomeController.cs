a2a7e2fa2f125a6be363a859cff5e7fd83a7924a
bbates01 <bradybates01@gmail.com>
2026-02-27 19:25:13 -0700

    update home controller

--- Mission08_Team0110/Controllers/HomeController.cs
+++ Mission08_Team0110/Controllers/HomeController.cs
@@ -1,18 +1,42 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-// using Mission08_Team0110.Models;
+using Mission08_Team0110.Models;
 
 namespace Mission08_Team0110.Controllers;
 
 public class HomeController : Controller
 {
+    private ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
         return View();
     }
-
+    
+    [HttpGet]
     public IActionResult Create()
     {
-        return View();
+        ViewBag.Categories = _context.Categories
+            .OrderBy(x => x.Name)
+            .ToList();
+        
+        return View(new TaskItem());
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var recordToEdit = _context.Tasks
+            .Single(x => x.TaskId == id);
+
+        ViewBag.Categories = _context.Categories
+            .OrderBy(x => x.Name)
+            .ToList();
+        
+        return View("Create", recordToEdit);
     }
 }
